32bfd7492ab10b08ae3ca839ef5ebd8bb58f8f31
Lin Weizhe Dragon <dvmsc110@gmail.com>
2018-12-01 09:17:27 +0000

    static files

--- src/website/learnah/learnah/settings.py
+++ src/website/learnah/learnah/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Cookie settings
 SESSION_COOKIE_NAME = 'ops_app'
